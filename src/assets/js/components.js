585e4f3ca0e950d79a76bbf70f9b544388cd6fa0
sherif-abouzaid-tt <152188935+sherif-abouzaid-tt@users.noreply.github.com>
2024-04-24 22:24:14 +0200

    start to build main

--- src/assets/js/components.js
+++ src/assets/js/components.js
@@ -3,4 +3,5 @@ $(document).ready(function() {
     $("#navbar").load("components/layout/navbar.html");
     $("#footer").load("components/layout/footer.html");
     $("#aside").load("components/layout/aside.html");
+    $("#main").load("components/main.html");
 });
